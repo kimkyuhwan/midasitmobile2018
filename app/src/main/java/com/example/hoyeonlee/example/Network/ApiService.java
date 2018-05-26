b330896b1a97ff1387ead41d567d965beceaecb8
yansfil <tansfil@gmail.com>
2018-05-26 17:43:28 +0900

    update ApiService.class

--- app/src/main/java/com/example/hoyeonlee/example/Network/ApiService.java
+++ app/src/main/java/com/example/hoyeonlee/example/Network/ApiService.java
@@ -3,23 +3,30 @@ package com.example.hoyeonlee.example.Network;
 import com.example.hoyeonlee.example.DataSchema.Menu;
 import com.example.hoyeonlee.example.DataSchema.Menus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 /**
  * Created by hoyeonlee on 2018. 3. 17..
  */
 
 public interface ApiService {
-    public static final String URL = "http://210.89.178.127:8000/";
+    public static final String URL = "http://192.168.0.60:8000/";
     public static final String subURL = "/api/v1/";
 
     @POST(subURL+"registration/")
@@ -33,5 +40,16 @@ public interface ApiService {
     Call<ResponseBody> test();
 
     @GET(subURL+"menus/")
-    Call<Menus> getMenu();
+    Call<ArrayList<Menu>> getMenu();
+
+    @POST(subURL+"menus/")
+    Call<Menu> addMenu(@Body RequestBody body);
+
+    @DELETE(subURL+"menus/{id}/")
+    Call<Menu> deleteMenu(@Path("id") String id);
+
+    @Headers({"Content-Type:text/xml"})
+    @PUT(subURL+"menus/{id}/")
+    Call<Menu> updateMenu(@Path("id") String id,@Body RequestBody body);
+
 }
