860e1aa199992c17ffd5f475fae0e9989b98b9a3
gyuhwan <gmarble94@gmail.com>
2018-05-26 19:45:50 +0900

    DataSchema Order, OrderList add

---
+++ app/src/main/java/com/example/hoyeonlee/example/DataSchema/Order.java
@@ -0,0 +1,124 @@
+package com.example.hoyeonlee.example.DataSchema;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+public class Order {
+
+    private int menu_id;
+    private String temperature;
+    private int count;
+    private String size;
+    private transient int price;
+    private transient String name;
+    public Order(int menu_id, int count,String temperature, String size) {
+        this.menu_id = menu_id;
+        this.temperature = temperature;
+        this.count = count;
+        this.size = size;
+    }
+
+
+    public void changeCount(int value){
+        count+=value;
+    }
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Order(String key, int cnt){
+
+        String value="";
+        count=cnt;
+        temperature=null;
+        size=null;
+        int state=0;
+        for(int i=0;i<key.length();i++){
+            if(key.charAt(i)=='#'){
+                if(!value.equals("")){
+                    switch (state){
+                        case 0: menu_id=Integer.parseInt(value); break;
+                        case 1: temperature=value; break;
+                        case 2: size=value; break;
+                        case 3: name=value; break;
+                        case 4: price=Integer.parseInt(value); break;
+                        default: break;
+                    }
+                }
+                value="";
+                state++;
+            }
+            else{
+                value+=key.charAt(i);
+            }
+        }
+        Log.d("DEUBGYU",getJSONString());
+    }
+
+    public int getId() {
+        return menu_id;
+    }
+
+    public void setId(int id) {
+        this.menu_id = id;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String makeKey(){
+        return String.valueOf(menu_id)+"#"+temperature+"#"+size+"#"+name+"#"+price+"#";
+    }
+
+    public String getJSONString(){
+        Gson gson=new Gson();
+        return gson.toJson(this);
+    }
+
+    public String getDetail(){
+        if(size==null || size.equals("null")) return "";
+        else if(temperature==null) return size;
+        return temperature+" / " + size;
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/hoyeonlee/example/DataSchema/OrderList.java
@@ -0,0 +1,115 @@
+package com.example.hoyeonlee.example.DataSchema;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class OrderList {
+    HashMap<String,Integer> order_map;
+    Iterator<Map.Entry<String, Integer> > it;
+
+    public int size(){
+        return order_map.size();
+    }
+
+    public void iter_init(){
+        it = order_map.entrySet().iterator();
+    }
+
+    public Order getOrder(int idx){
+        iter_init();
+        if(order_map.size()<idx) return null;
+        for(int i=0;i<idx;i++){
+            iter_next();
+        }
+        return iter_next();
+    }
+
+    public void changeValue(int idx, int value){
+        Order temp=getOrder(idx);
+        String key=temp.makeKey();
+        int cnt=temp.getCount();
+        order_map.put(key,cnt+value);
+    }
+    public Order iter_next(){
+        if(!it.hasNext()) iter_init();
+        Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) it.next();
+        System.out.println(pair.getKey() + " = " + pair.getValue());
+        Order here=new Order(pair.getKey(),pair.getValue());
+        return here;
+    }
+
+    public int getTotalPrice(){
+        int ret=0;
+        Iterator<Map.Entry<String, Integer>> it = order_map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            Log.d("DEBUGYU",pair.getKey() + " = " + pair.getValue());
+
+            Order here=new Order(pair.getKey(),pair.getValue());
+            ret+=here.getCount()*here.getPrice();
+        }
+        return ret;
+    }
+
+
+    public OrderList() {
+        order_map=new HashMap<String, Integer>();
+    }
+    public void addData(Order order){
+        int cnt=order.getCount();
+        String key=order.makeKey();
+        Log.d("DEBUGYU",key+", "+cnt);
+        if(order_map.containsKey(key)){
+            Integer temp=order_map.get(key);
+            temp+=cnt;
+            order_map.put(key,temp);
+        }
+        else{
+            order_map.put(key,cnt);
+        }
+        getJSONOrderList();
+    }
+    public void removeData(Order order){
+        int cnt=order.getCount();
+        String key=order.makeKey();
+        if(order_map.containsKey(key)){
+            order_map.remove(key);
+        }
+    }
+
+    public void clear(){
+        order_map.clear();
+    }
+
+    public String getJSONOrderList(){
+        if(order_map==null){
+            return null;
+        }
+        ArrayList<Order> orderArrayList=new ArrayList<>();
+        Iterator<Map.Entry<String, Integer>> it = order_map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>) it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            Log.d("DEBUGYU",pair.getKey() + " = " + pair.getValue());
+
+            Order here=new Order(pair.getKey(),pair.getValue());
+            orderArrayList.add(here);
+        }
+        Gson gson=new Gson();
+        String ret=gson.toJson(orderArrayList);
+        Log.d("DEBUGYU",ret);
+        return ret;
+    }
+}
