2718bc40e2cfda49ed8bd78aa5528eb59aa3f241
gyuhwan <gmarble94@gmail.com>
2018-05-26 10:00:08 +0900

    Login Activiy - Layout (butterknife Binding)

--- app/src/main/java/com/example/hoyeonlee/example/Login/SignInActiviy.java
+++ app/src/main/java/com/example/hoyeonlee/example/Login/SignInActiviy.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +20,9 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,48 +30,59 @@ import retrofit2.Response;
 
 public class SignInActiviy extends AppCompatActivity {
 
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.input_email)
+    EditText inputEmail;
+    @BindView(R.id.input_password)
+    EditText inputPassword;
+    @BindView(R.id.btn_signin)
+    Button btnSignin;
+    @BindView(R.id.btn_signup)
+    Button btnSignup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-        //Toolbar Title정하기
-        ((TextView) findViewById(R.id.tv_title)).setText("로그인");
-        final EditText emailInput = findViewById(R.id.input_email);
-        final EditText pwInput = findViewById(R.id.input_password);
+        ButterKnife.bind(this);
+        tvTitle.setText("로그인");
 
         //로그인 여부 확인
-        if(SharedPreferenceBase.getSharedPreference("login") == true){
-            startActivity(new Intent(this,MainActivity.class));
+        if (SharedPreferenceBase.getSharedPreference("login") == true) {
+            startActivity(new Intent(this, MainActivity.class));
             finish();
         }
+    }
 
-        findViewById(R.id.btn_signin).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @OnClick({R.id.btn_signin, R.id.btn_signup})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_signin:
                 MApplication.getInstance().getApiService()
-                        .signIn(emailInput.getText().toString(),pwInput.getText().toString())
+                        .signIn(inputEmail.getText().toString(), inputPassword.getText().toString())
                         .enqueue(new Callback<ResponseBody>() {
                             @Override
                             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                                 String code = String.valueOf(response.code());
-                                if(code.charAt(0) == '4'|| code.charAt(0) == '5'){
-                                    if(code.equals("400")){
+                                if (code.charAt(0) == '4' || code.charAt(0) == '5') {
+                                    if (code.equals("400")) {
                                         Toast.makeText(SignInActiviy.this, "아이디 비밀번호를 다시 입력해주세요", Toast.LENGTH_SHORT).show();
                                         return;
                                     }
-                                    Toast.makeText(SignInActiviy.this, code+" Error", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(SignInActiviy.this, code + " Error", Toast.LENGTH_SHORT).show();
                                     return;
                                 }
                                 try {
                                     //토큰 저장하기
                                     JSONObject result = new JSONObject(response.body().string());
-                                    SharedPreferenceBase.putSharedPreference(Preferences.SHARED_PREFERENCE_NAME_COOKIE,result.getString("key"));
+                                    SharedPreferenceBase.putSharedPreference(Preferences.SHARED_PREFERENCE_NAME_COOKIE, result.getString("key"));
                                     SharedPreferenceBase.putSharedPreference("login", true);
                                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
                                     finish();
                                 } catch (IOException e) {
                                     e.printStackTrace();
-                                } catch (JSONException e){
+                                } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
                             }
@@ -77,37 +92,10 @@ public class SignInActiviy extends AppCompatActivity {
                                 Toast.makeText(SignInActiviy.this, t.getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         });
-
-            }
-        });
-        findViewById(R.id.btn_signup).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),SignUpActiviy.class));
-            }
-        });
-
-        //토큰 테스트 코드
-
-//        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                MApplication.getInstance().getApiService().test().enqueue(new Callback<ResponseBody>() {
-//                    @Override
-//                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                        try {
-//                            Toast.makeText(SignInActiviy.this, response.body().string(), Toast.LENGTH_SHORT).show();
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-//
-//                    }
-//                });
-//            }
-//        });
+                break;
+            case R.id.btn_signup:
+                startActivity(new Intent(getApplicationContext(), SignUpActiviy.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/hoyeonlee/example/Login/SignUpActiviy.java
+++ app/src/main/java/com/example/hoyeonlee/example/Login/SignUpActiviy.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.hoyeonlee.example.*;
+import com.example.hoyeonlee.example.MApplication;
 import com.example.hoyeonlee.example.Network.SharedPreferenceBase;
+import com.example.hoyeonlee.example.R;
 import com.example.hoyeonlee.example.Utils.Preferences;
 
 import org.json.JSONException;
@@ -17,6 +19,9 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,65 +29,74 @@ import retrofit2.Response;
 
 public class SignUpActiviy extends AppCompatActivity {
 
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.input_email)
+    EditText inputEmail;
+    @BindView(R.id.input_password)
+    EditText inputPassword;
+    @BindView(R.id.input_password_re)
+    EditText inputPasswordRe;
+    @BindView(R.id.input_name)
+    EditText inputName;
+    @BindView(R.id.btn_complete)
+    Button btnComplete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
         //Toolbar Title정하기
-        ((TextView) findViewById(R.id.tv_title)).setText("회원가입");
-        final EditText emailInput = findViewById(R.id.input_email);
-        final EditText pwInput = findViewById(R.id.input_password);
-        final EditText pwInput_re = findViewById(R.id.input_password_re);
+        tvTitle.setText("회원가입");
+        // TODO 이름 검증에 대한 내용이 없습니다.
+    }
 
-        findViewById(R.id.btn_complete).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //아이디 검증
-                if(emailInput.getText().toString().trim().length() == 0){
-                    Toast.makeText(SignUpActiviy.this, "아이디를 제대로 입력해주세요", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                //비밀번호 검증 및 자리수 검증
-                if(pwInput.getText().toString().trim().length() >= 8
-                        || pwInput.getText().toString().equals(pwInput_re.getText().toString())){
-                    Toast.makeText(SignUpActiviy.this, "비밀번호를 다시 입력해주세요", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                MApplication.getInstance().getApiService()
-                        .signUp(emailInput.getText().toString(),pwInput.getText().toString(),pwInput_re.getText().toString())
-                        .enqueue(new Callback<ResponseBody>() {
-                            @Override
-                            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                                String code = String.valueOf(response.code());
-                                if(code.charAt(0) == '4'|| code.charAt(0) == '5'){
-                                    if(code.equals("400")){
-                                        Toast.makeText(getApplicationContext(), "아이디 비밀번호를 다시 입력해주세요", Toast.LENGTH_SHORT).show();
-                                        return;
-                                    }
-                                    Toast.makeText(getApplicationContext(), code+" Error", Toast.LENGTH_SHORT).show();
-                                    return;
-                                }
-                                try {
-                                    JSONObject result = new JSONObject(response.body().string());
-                                    SharedPreferenceBase.putSharedPreference(Preferences.SHARED_PREFERENCE_NAME_COOKIE,result.getString("key"));
-                                    SharedPreferenceBase.putSharedPreference("login", true);
-                                    //SignIn , SignUp Activity 모두 없애고 로그인
-                                    Intent intent = new Intent(getApplicationContext(), SignInActiviy.class);
-                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    startActivity(intent);
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                } catch (JSONException e){
-                                    e.printStackTrace();
-                                }
+    @OnClick(R.id.btn_complete)
+    public void onViewClicked() {
+        if (inputEmail.getText().toString().length() == 0) {
+            Toast.makeText(SignUpActiviy.this, "아이디를 제대로 입력해주세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //비밀번호 검증 및 자리수 검증
+        if (inputPassword.getText().toString().length() < 8
+                || !inputPassword.getText().toString().equals(inputPasswordRe.getText().toString())) {
+            Toast.makeText(SignUpActiviy.this, "비밀번호를 다시 입력해주세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        MApplication.getInstance().getApiService()
+                .signUp(inputEmail.getText().toString(), inputPassword.getText().toString(), inputPasswordRe.getText().toString())
+                .enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        String code = String.valueOf(response.code());
+                        if (code.charAt(0) == '4' || code.charAt(0) == '5') {
+                            if (code.equals("400")) {
+                                Toast.makeText(getApplicationContext(), "아이디 비밀번호를 다시 입력해주세요", Toast.LENGTH_SHORT).show();
+                                return;
                             }
+                            Toast.makeText(getApplicationContext(), code + " Error", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        try {
+                            JSONObject result = new JSONObject(response.body().string());
+                            SharedPreferenceBase.putSharedPreference(Preferences.SHARED_PREFERENCE_NAME_COOKIE, result.getString("key"));
+                            SharedPreferenceBase.putSharedPreference("login", true);
+                            //SignIn , SignUp Activity 모두 없애고 로그인
+                            Intent intent = new Intent(getApplicationContext(), SignInActiviy.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
-                            @Override
-                            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                                Toast.makeText(SignUpActiviy.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
-        });
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Toast.makeText(SignUpActiviy.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
